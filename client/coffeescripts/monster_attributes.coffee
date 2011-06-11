4978d1dc5db607ced0d36fb25a835198813bce1d
Michael Morris <mtcmorris@gmail.com>
2011-06-12 09:24:56 +1000

    Starting on monster attributes

---
+++ client/coffeescripts/monster_attributes.coffee
@@ -0,0 +1,8 @@
+monsters = [
+  {
+    name: "goblin",
+    strength: 1,
+    health: 100,
+    sprites: [""]
+  }
+]
---
+++ client/javascripts/monster_attributes.js
@@ -0,0 +1,11 @@
+(function() {
+  var monsters;
+  monsters = [
+    {
+      name: "goblin",
+      strength: 1,
+      health: 100,
+      sprites: [""]
+    }
+  ];
+}).call(this);
