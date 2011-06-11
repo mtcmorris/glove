6e8b6d7b1d8d3cc402be3bb97d5a3551b5c4774f
Gabe <gabe@avantbard.com>
2011-06-11 18:05:10 +1000

    adding crafty docs

---
+++ reference/craftyjs.com/contact.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Crafty - JavaScript Game Engine</title>
+<link type="text/css" rel="stylesheet" href="global.css"/>
+<link href='http://fonts.googleapis.com/css?family=Arvo:regular,bold' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="favicon.ico"> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div id="header">
+	<div id="inner">
+	<a href="http://craftyjs.com"><img id="logo" src="images/logo.png" alt="Crafty - JavaScript Game Engine" /></a>
+	<ul>
+		<li><a href="api/">Documentation</a></li>
+		<li><a href="tutorials.php">Tutorial</a></li>
+		<li><a href="demos.php">Demos</a></li>
+		<!-- <li><a href="#comingsoon">Builder<sup>new</sup></a></li> -->
+		<li><a href="http://groups.google.com/group/craftyjs">Forums</a></li>
+		<li><a href="http://craftyjs.tumblr.com/">Blog</a></li>
+		<!-- <li><a href="api">FAQ</a></li>-->
+		<li class="emph"><a href="download.php">Download</a></li>
+	</ul>
+	</div>
+</div>
+<div id="main">
+<div id="main-left"></div>
+<div id="main-right"></div>
+<div id="content">
+<div id="left">
+	<h1>Contact</h1>
+	<p>You can contact Louis Stowasser, the project admin, via email at 
+	<a href="mailto:louis@craftyjs.com">louis@craftyjs.com</a> or <a href="mailto:louisstow@gmail.com">louisstow@gmail.com</a>		or follow <a href="http://twitter.com/#!/craftyjs">tweets</a> and the more frequent <a href="http://craftyjs.tumblr.com/">tumblr blog</a>.	</p>
+</div>
+<div id="right">
+	<p><a href="mailto:louis@craftyjs.com">louis@craftyjs.com</a></p>
+	<p><a href="mailto:louisstow@gmail.com">louisstow@gmail.com</a></p>		<p><a href="http://twitter.com/#!/craftyjs">Twitter</a></p>		<p><a href="http://craftyjs.tumblr.com/">Tumblr</a></p>
+</div>
+
+</div>
+</div>
+
+<div class="clearer"></div>
+<div id="footer">
+
+<div id="contact">
+	<a href="http://twitter.com/craftyjs"><img src="images/twitter.png"/> @craftyjs</a>
+	<a href="http://webchat.freenode.net"><img src="images/comment.png"/> #crafty@freenode.net</a>
+	<a href="http://groups.google.com/group/craftyjs"><img src="images/google.png"/> google groups</a>
+	<a href="mailto:louis@craftyjs.com"><img src="images/email.png"/> louis@craftyjs.com</a>
+	<a href="https://github.com/louisstow/Crafty"><img src="images/github.png"/> github</a>
+</div>
+
+<ul>
+	<li><a href="api/">Documentation</a></li>
+	<li><a href="tutorials.php">Tutorial</a></li>
+	<li><a href="demos.php">Demos</a></li>
+	<!-- <li><a href="#comingsoon">Builder<sup>new</sup></a></li> -->
+	<li><a href="forums/">Forums</a></li>
+	<li><a href="http://craftyjs.tumblr.com/">Blog</a></li>
+	<!-- <li><a href="api">FAQ</a></li>-->
+	<li><a href="download.php">Download</a></li>
+</ul>
+<p>&copy; Crafty 2010. Crafty is distributed under the <a href="http://en.wikipedia.org/wiki/MIT_License">MIT</a> or 
+<a href="http://en.wikipedia.org/wiki/GNU_General_Public_License">GPL</a> license.</p>
+</div>
+</body>
+</html>
---
+++ reference/craftyjs.com/demos.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Crafty - JavaScript Game Engine</title>
+<link type="text/css" rel="stylesheet" href="global.css"/>
+<link href='http://fonts.googleapis.com/css?family=Arvo:regular,bold' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="favicon.ico"> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div id="header">
+	<div id="inner">
+	<a href="http://craftyjs.com"><img id="logo" src="images/logo.png" alt="Crafty - JavaScript Game Engine" /></a>
+	<ul>
+		<li><a href="api/">Documentation</a></li>
+		<li><a href="tutorials.php">Tutorial</a></li>
+		<li><a href="demos.php">Demos</a></li>
+		<!-- <li><a href="#comingsoon">Builder<sup>new</sup></a></li> -->
+		<li><a href="http://groups.google.com/group/craftyjs">Forums</a></li>
+		<li><a href="http://craftyjs.tumblr.com/">Blog</a></li>
+		<!-- <li><a href="api">FAQ</a></li>-->
+		<li class="emph"><a href="download.php">Download</a></li>
+	</ul>
+	</div>
+</div>
+<div id="main">
+<div id="main-left"></div>
+<div id="main-right"></div>
+<div id="content"><div id="wide">
+<h1>Demos</h1>
+
+<div class="demo">
+<img src="demos/thumbs/fruit.png"/>
+<h2><a href="demos/fruitassassin" target="_blank">Fruit Assassin</a></h2>
+
+<p>The famous <a href="http://www.fruitninja.com/">FruitNinja</a> in HTML5.</p>
+<div class="clearer"></div>
+</div>
+
+<div class="demo">
+<img src="demos/thumbs/isometric.png"/>
+<h2><a href="demos/isometric" target="_blank">Isometric Test</a></h2>
+
+<p>Using the Isometric placement and polygon click maps.</p>
+<div class="clearer"></div>
+</div>
+
+<div class="demo">
+<img src="demos/thumbs/connect4.png"/>
+<h2><a href="demos/connect4" target="_blank">Connect 4</a></h2>
+
+<p>Puzzle game Connect 4. Two player but no AI.</p>
+<div class="clearer"></div>
+</div>
+
+<div class="demo">
+<img src="demos/thumbs/gridshock.png"/>
+<h2><a href="http://bowlerhatgames.com/html-games/gridshock/" target="_blank">Gridshock</a></h2>
+
+<p>Match three or more lights, in any direction, and don't let the grid fill up!</p>
+<div class="clearer"></div>
+</div>
+
+<div class="demo">
+<img src="demos/thumbs/asteroids.png"/>
+<h2><a href="demos/asteroids" target="_blank">Asteroids</a></h2>
+
+<p>Full screen Asteroids.</p>
+<div class="clearer"></div>
+</div>
+
+<div class="demo">
+<img src="demos/thumbs/rpg.png"/>
+<h2><a href="demos/tutorial" target="_blank">Basic RPG</a></h2>
+
+<p>Basic elements of an RPG with a <a href="demos/tutorial/tutorial.html" target="_blank">tutorial</a>. Only 160 lines of Crafty code!</p>
+<div class="clearer"></div>
+</div>
+
+
+<div class="demo">
+<img src="demos/thumbs/javasquirrel.png"/>
+<h2><a href="http://twablet.com/javasquirrel/" target="_blank">JavaSquirrel</a></h2>
+
+<p>Collect the acorns. Written by <a href="http://twitter.com/#!/eric_garside" target="_blank">Eric Garside</a>.</p>
+<div class="clearer"></div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="clearer"></div>
+<div id="footer">
+
+<div id="contact">
+	<a href="http://twitter.com/craftyjs"><img src="images/twitter.png"/> @craftyjs</a>
+	<a href="http://webchat.freenode.net"><img src="images/comment.png"/> #crafty@freenode.net</a>
+	<a href="http://groups.google.com/group/craftyjs"><img src="images/google.png"/> google groups</a>
+	<a href="mailto:louis@craftyjs.com"><img src="images/email.png"/> louis@craftyjs.com</a>
+	<a href="https://github.com/louisstow/Crafty"><img src="images/github.png"/> github</a>
+</div>
+
+<ul>
+	<li><a href="api/">Documentation</a></li>
+	<li><a href="tutorials.php">Tutorial</a></li>
+	<li><a href="demos.php">Demos</a></li>
+	<!-- <li><a href="#comingsoon">Builder<sup>new</sup></a></li> -->
+	<li><a href="forums/">Forums</a></li>
+	<li><a href="http://craftyjs.tumblr.com/">Blog</a></li>
+	<!-- <li><a href="api">FAQ</a></li>-->
+	<li><a href="download.php">Download</a></li>
+</ul>
+<p>&copy; Crafty 2010. Crafty is distributed under the <a href="http://en.wikipedia.org/wiki/MIT_License">MIT</a> or 
+<a href="http://en.wikipedia.org/wiki/GNU_General_Public_License">GPL</a> license.</p>
+</div>
+</body>
+</html>
